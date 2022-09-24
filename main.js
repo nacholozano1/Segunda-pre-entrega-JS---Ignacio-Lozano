2e13685d7a3a3155e1d4b325b24a5f8b2bc2222f
Ignacio Lozano <nacholozano1@gmail.com>
2022-09-24 17:24:04 -0300

    v2.0

--- main.js
+++ main.js
@@ -2,31 +2,28 @@ alert("Bienvenido a la calculadora de impuestos de los juegos de steam");
 
 const autorizacion = "empezar";
 
-let usuario = prompt("Ingrese 'empezar' para iniciar");
 
-for( let i = 0; i < 1; i++){
+let usuario = prompt("Ingrese 'empezar' para iniciar");
     if (usuario == autorizacion){
         alert("Bienvenido!")
+        
         let precioJuego = parseInt(prompt("Ingrese el precio del juego:"));
-        for(let i = 0; i < precioJuego; i++) {
-            let precio = calcularImpuesto(precioJuego);
-            alert("El precio total del juego es: " + precio);
-            alert("Usted está siendo robado!");
-        }
-        if (precioJuego) NaN;
-        {
-            alert("Pone un número durazno. Dale refresh para volver a intentarlo ;)")
-            break;
-        } 
 
-    } else (usuario == autorizacion) 
-    {
+        for(let i = 0; i < precioJuego; i++) {
+            let precioFinal = calcularImpuesto(precioJuego);
+            alert("El precio total del juego es: " + precioFinal);
+            alert("Usted está siendo robado!");
+            break;
+        }
+    } 
+    
+    while (usuario != autorizacion){
         alert("Tenés que escribir 'empezar' crack, no " + "'" + usuario + "'" + ". Dale al refresh para probar de nuevo ;)");
-    }
-}
+        break;
+        } 
 
 function calcularImpuesto(precioJuego)
 {
-    let precio = (precioJuego) * 1.75;
-    return precio;
+    let precioFinal = (precioJuego) * 1.75;
+    return precioFinal;
 }
